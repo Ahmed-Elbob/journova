82faeedf9d91d32c23f0fdd19c22518ed0f40548
Ahmed-Elbob <ae328644@gmail.com>
2025-04-27 23:53:16 +0300

    home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,8 +12,8 @@ export default function Home() {
 
   return (
     <>
-    <MainSlider/>
-<CategoriesSlider/>
+    {/* <MainSlider/> */}
+{/* <CategoriesSlider/> */}
     <RecentProducts/>
     </>
    
